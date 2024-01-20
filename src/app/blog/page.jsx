9a8c923facec3c47fe5509be64c6f8f6ea04b3ef
COREVE\rizzardit <t.rizzardi@ns12.it>
2024-01-20 13:13:06 +0100

    first commit

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -23,7 +23,19 @@ const Blog = async () => {
       <div className={styles.mainContainer}>
         {data.map((item) => (
           <Link href={`/blog/${item._id}`} className={styles.container} key={item._id}>
-            {/* ... Resto del codice ... */}
+             <div className={styles.imageContainer}>
+            <Image
+              src={item.img}
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.desc}</p>
+          </div>
           </Link>
         ))}
       </div>
