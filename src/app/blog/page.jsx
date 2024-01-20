bfb2740edd2b8f9caefa1f04bed2529721eef5b0
COREVE\rizzardit <t.rizzardi@ns12.it>
2024-01-20 12:05:10 +0100

    first deploy

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -9,35 +9,30 @@ async function getData() {
   });
 
   if (!res.ok) {
-    throw new Error("Failed to fetch data");
+    throw new Error("Failed to fetch data API");
   }
 
   return res.json();
 }
 
 const Blog = async () => {
-  const data = await getData();
-  return (
-    <div className={styles.mainContainer}>
-      {data.map((item) => (
-        <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
-          <div className={styles.imageContainer}>
-            <Image
-              src={item.img}
-              alt=""
-              width={400}
-              height={250}
-              className={styles.image}
-            />
-          </div>
-          <div className={styles.content}>
-            <h1 className={styles.title}>{item.title}</h1>
-            <p className={styles.desc}>{item.desc}</p>
-          </div>
-        </Link>
-      ))}
-    </div>
-  );
+  try {
+    const data = await getData();
+
+    return (
+      <div className={styles.mainContainer}>
+        {data.map((item) => (
+          <Link href={`/blog/${item._id}`} className={styles.container} key={item._id}>
+            {/* ... Resto del codice ... */}
+          </Link>
+        ))}
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+    // Puoi gestire l'errore in modo appropriato, ad esempio, mostrando un messaggio di errore nell'interfaccia utente.
+    return <div>Error fetching data. Please try again later.</div>;
+  }
 };
 
 export default Blog;
